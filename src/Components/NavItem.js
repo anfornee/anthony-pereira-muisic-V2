f1396d01ccb0b684d07cfcecf4060998c01ee42c
anfornee <anthonyp@ichop.org>
2020-10-15 16:09:26 -0700

    linter happiness level increase

--- src/Components/NavItem.js
+++ src/Components/NavItem.js
@@ -1,31 +1,28 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const NavItem = ({ location, setLocation, item, defaultStyle, activeStyle, addHovered, removeHovered }) => {
-
-  return (
-    <div
-      style={
-        location === '/'
-          ? defaultStyle
-          : location === `/${item}`
-            ? { top: 0, opacity: 1 } : { top: 0, opacity: 0, zIndex: -1 }
-      }
-      className='navlink-container'
-      onClick={() => setLocation(`/${item}`)}
-    >
-      <Link to={`/${item}`}>
-        <h2
-          id={`nav-${item}`}
-          onMouseEnter={e => addHovered(e)}
-          onMouseLeave={e => removeHovered(e)}
-        >
-          {item.toUpperCase()}
-        </h2>
-      </Link>
-      <div id={`${item}-line`} className={location === `/${item}` ? `${item}-hover-line ${item}-nav-active` : `${item}-hover-line`} />
-    </div>
-  )
-}
+const NavItem = ({ location, setLocation, item, defaultStyle, activeStyle, addHovered, removeHovered }) => (
+  <div
+    style={
+      location === '/'
+        ? defaultStyle
+        : location === `/${item}`
+          ? { top: 0, opacity: 1 } : { top: 0, opacity: 0, zIndex: -1 }
+    }
+    className='navlink-container'
+    onClick={() => setLocation(`/${item}`)}
+  >
+    <Link to={`/${item}`}>
+      <h2
+        id={`nav-${item}`}
+        onMouseEnter={e => addHovered(e)}
+        onMouseLeave={e => removeHovered(e)}
+      >
+        {item.toUpperCase()}
+      </h2>
+    </Link>
+    <div id={`${item}-line`} className={location === `/${item}` ? `${item}-hover-line ${item}-nav-active` : `${item}-hover-line`} />
+  </div>
+)
 
 export default NavItem
