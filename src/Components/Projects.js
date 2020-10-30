119ee9e6e8572713b34be453f9ac961cd8e53aad
anfornee <anthonyp@ichop.org>
2020-10-29 22:13:11 -0700

    Starting to work on custom audio player for albums

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { useRef } from 'react'
 
 const Projects = props => {
+  const track1 = useRef(null)
+  const playSong = () => track1.current.play()
+  const pauseSong = () => track1.current.pause()
+
   return (
-    <h1>Projects</h1>
+    <div>
+      <h1>Projects</h1>
+      <audio
+        ref={track1}
+        controlsList='nodownload'
+      >
+        <source src='https://docs.google.com/uc?export=&id=1v6hDnzUSxMNKbkRO40PvqKIfV1sU8S6j' />
+      </audio>
+      <button onClick={playSong}>
+        PLAY
+      </button>
+      <button onClick={pauseSong}>
+        PAUSE
+      </button>
+    </div>
   )
 }
+
 export default Projects
