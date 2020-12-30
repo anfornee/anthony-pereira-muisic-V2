1d3665679087f9c879bac37453dbe601bf558dd5
anfornee <anthonyp@ichop.org>
2020-12-29 17:38:15 -0800

    Getting latest page rockin

--- src/Components/Latest.js
+++ src/Components/Latest.js
@@ -1,7 +1,11 @@
 import React from 'react'
 
 const Latest = props => (
-  <div>LATEST</div>
+  <div id='latest'>
+    <p class='latest-text roboto'>
+      Make sure to come back here for more content and updates!
+    </p>
+  </div>
 )
 
 export default Latest
