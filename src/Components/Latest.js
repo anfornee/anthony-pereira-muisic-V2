40c5a45f05b411ed4475fe0b6000372f6fd15fc6
anfornee <anthonyp@ichop.org>
2021-01-26 21:05:03 -0800

    Just updated some text on Latest.

--- src/Components/Latest.js
+++ src/Components/Latest.js
@@ -5,7 +5,7 @@ const Latest = ({ images }) => (
     <p className='latest-text roboto'>
       <a href='https://www.patreon.com/posts/hello-46624431?utm_medium=clipboard_copy&utm_source=copy_to_clipboard&utm_campaign=postshare'>
         <img className='latest-img' src={images.patreonPost} alt='Patreon Post' />
-        <span className='latest-img-desc'>Check out my most recent post on my Patreon</span>
+        <span className='latest-img-desc'>Check out my most recent post on Patreon</span>
       </a>
     </p>
   </div>
