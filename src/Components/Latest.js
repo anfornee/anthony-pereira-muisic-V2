5ac9cee223cb3e0af8514093922489d31448b377
anfornee <anthonyp@ichop.org>
2021-03-15 10:35:41 -0700

    New latest post

--- src/Components/Latest.js
+++ src/Components/Latest.js
@@ -3,9 +3,9 @@ import React from 'react'
 const Latest = ({ images }) => (
   <div id='latest'>
     <p className='latest-text roboto'>
-      <a href='https://youtu.be/-M6kb3JoFZM'>
+      <a href='https://youtu.be/HfqYuDNvcuE'>
         <img className='latest-img' src={images.latestPost} alt='Latest Post' />
-        <span className='latest-img-desc'>Check out the latest addition to Life Cues. Breeze 0207.</span>
+        <span className='latest-img-desc'>I got my first job as a composer! Watch the story on Youtube.</span>
       </a>
     </p>
   </div>
