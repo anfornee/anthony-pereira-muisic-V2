17b9ce05a516ab9712ae647cc711d03e360dd7d8
anfornee <anthonyp@ichop.org>
2021-07-26 10:38:48 -0700

    New video

--- src/Components/Latest.js
+++ src/Components/Latest.js
@@ -3,10 +3,10 @@ import React from 'react'
 const Latest = ({ images }) => (
   <div id='latest' className='load-content'>
     <p className='latest-text roboto' >
-      <a href='https://youtu.be/chFjs4TOHJ0'>
+      <a href='https://youtu.be/B3zWaqTqYyo'>
         <img className='latest-img' src={images.latestPost} alt='Latest Post' />
         <span className='latest-img-desc'>
-          I wrote an original score for a scoring competition from producer, Jon Meyer. Check it out here.
+          I did a track rundown for my recent original score. Check it out here.
         </span>
       </a>
     </p>
